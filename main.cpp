5e96a890c6c98c7ad634b507d1913716662ab93c
Eva <evamajorova@yandex.ru>
2022-12-15 17:02:13 +0300

    tests added

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 template <class T>
-void print(vector<T> v)
+void print(vector<T> &v)
 {
     for (int i = 0; i < v.size(); ++i)
         std::cout << v[i] << ' ';
@@ -23,6 +23,7 @@ std::ostream& operator<<(std::ostream& os, const test&t)
 {
     return os << t.x << ' ' << t.c << ' ' << t.b;
 }
+
 
 int main()
 {
@@ -47,28 +48,94 @@ int main()
     vector<test> v18{3, t};  
     vector<test> v19{t};
 
+    std::cout << "v1: ";
+    print(v1);
+    std::cout << "v2: ";
+    print(v2);
+    std::cout << "v3: ";
+    print(v3);
+    std::cout << "v4: ";
+    print(v4);
+    std::cout << "v5: ";
+    print(v5);
+    std::cout << "v6: ";
+    print(v6);
+    std::cout << "v7: ";
+    print(v7);
+    std::cout << "v8: ";
+    print(v8);
+    std::cout << "v9: ";
+    print(v9);
+    std::cout << "v10: ";
+    print(v10);
+    std::cout << "v11: ";
+    print(v11);
+    std::cout << "v12: ";
+    print(v12);
+    std::cout << "v13: ";
+    print(v13);
+    std::cout << "v14: ";
+    print(v14);
+    std::cout << "v15: ";
+    print(v15);
+    std::cout << "v16: ";
+    print(v16);
+    std::cout << "v17: ";
+    print(v17);
+    std::cout << "v18: ";
+    print(v18);
+    std::cout << "v19: ";
+    print(v19);
+
+    v1 = v2;
+    v2 = vector<int> {4};
+
+    std::cout << "v1, v2:\n"; 
     print(v1);
     print(v2);
-    print(v3);
-    print(v4);
-    print(v5);
-    print(v6);
-    print(v7);
-    print(v8);
-    print(v9);
-    print(v10);
-    print(v11);
-    print(v12);
-    print(v13);
-    print(v14);
-    print(v15);
-    print(v16);
-    print(v17);
-    print(v18);
-    print(v19);
+
+    std::cout << '\n';
+    vector<int> g(12, 12);
+    vector<int> c = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::cout << "g: ";
+    print(g);
+    g.push_back(5);
+    std::cout << "space: " << g.capacity() << '\n';
+    std::cout << "g, c:\n";
+    print(g);
+    print(c);
+
+    vector<test> empty;
+    vector<int> empty1(0);
+
+    std::cout << "empty, empty1:\n"; 
+    print(empty);
+    print(empty1);
+
+    vector<int> copy(v9);
+    vector<std::string> copy1(v6);
+    vector<test> copy2(v18);
+
+    std::cout << "copy, copy1, copy2:\n"; 
+    print(copy);
+    print(copy1);
+    print(copy2);
+
+    std::cout << copy1.at(1) << '\n';  
+    //std::cout << copy1.at(2);
+
+    std::cout << copy1[1] << '\n';  
+    std::cout << v13[2] << '\n';
+    //std::cout << v13[5] << '\n';
+
+    vector<std::string> s1{"123", "ert", "tyu"};
+    //std::cout << "s1:\n";
+    //print(s1);
+
+    s1.push_back("56293");
+    std::cout << "s1:\n";
+    print(s1);
+
+
     return 0;
 }
-
-#include <vector>
-
-//std::vector<int> v;
--- vector.h
+++ vector.h
@@ -4,66 +4,64 @@
 #include <algorithm>
 #include <stdexcept>
 
-
-template<class T, class A>
+template <class T, class A>
 struct vector_base
 {
     A alloc;
     int sz;
-    T* elem;
+    T *elem;
     int space;
 
-    vector_base(const A& a, int n)
-        :alloc{a}, elem{a.allocate(n)}, sz{n}, space{n}{}
-    vector_base(int n, const A& a)
-        :alloc{a}, elem{a.allocate(n)}, sz{n}, space{n}{}
+    vector_base(const A &a, int n)
+        : alloc{a}, elem{alloc.allocate(n)}, sz{n}, space{0} {}
     vector_base()
-        : elem{nullptr}, sz{0}, space{0}
-        {};
+        : elem{nullptr}, sz{0}, space{0} {}
     ~vector_base() { alloc.deallocate(elem, space); }
 };
 
-template<class T, class A = std::allocator<T>> 
+template <class T, class A = std::allocator<T>>
 class vector : private vector_base<T, A>
-//class vector
 {
     A alloc;
     int sz;
-    T* elem;
+    T *elem;
     int space;
+
 public:
-    vector(): sz{0}, elem{nullptr}, space{0} {}
+    vector() : sz{0}, elem{nullptr}, space{0} {}
 
     explicit vector(int s, T val = T{})
-    :sz{s}, elem{new T[s]}, space{s}
+        : sz{s}, elem{new T[sz]}, space{s}
     {
         for (int i = 0; i < s; ++i)
             elem[i] = val;
-            //elem[i] = 0;
     }
 
-    vector (const vector&);
-    vector<T, A>& operator=(const vector&);
+    vector(const vector &);
+    vector<T, A> &operator=(const vector &);
 
-    vector(vector&& a);
-    vector& operator=(vector&&);
+    vector(vector &&a);
+    vector &operator=(vector &&);
 
+    T &at(int n);
+    const T &at(int n) const;
 
-    T& at(int n);
-    const T& at(int n) const;
-
-    const T& operator[](int n) const
-        { return elem[n]; }
-    T& operator[](int n)
-        { return (elem[n]); }
+    const T &operator[](int n) const
+    {
+        return elem[n];
+    }
+    T &operator[](int n)
+    {
+        return (elem[n]);
+    }
 
     vector(std::initializer_list<T> lst)
-        :sz{lst.size()}, elem {new T[sz]}
+        : sz{lst.size()}, elem{new T[sz]}
     {
         std::copy(lst.begin(), lst.end(), elem);
     }
     ~vector() { delete[] elem; }
-    int size () const { return sz; }
+    int size() const { return sz; }
 
     T get(int n) const { return elem[n]; }
     void set(int n, T v) { elem[n] = v; }
@@ -72,27 +70,26 @@ public:
     int capacity() const;
     void resize(int newsize, T val = T());
 
-    void push_back(const T& d);
+    void push_back(const T &d);
 };
 
-
-template<class T, class A>
-vector<T, A>::vector(const vector& arg)
-    :sz{arg.sz}, elem(new T[arg.sz])
+template <class T, class A>
+vector<T, A>::vector(const vector &arg)
+    : sz{arg.sz}, elem(new T[arg.sz])
 {
     std::copy(arg.elem, arg.elem + arg.sz, elem);
 }
 
-template<class T, class A>
-vector<T, A>::vector(vector&& a)
-    :sz{a.sz}, elem{a.elem}
-    {
-        a.sz = 0;
-        a.elem = nullptr;
-    }
+template <class T, class A>
+vector<T, A>::vector(vector &&a)
+    : sz{a.sz}, elem{a.elem}
+{
+    a.sz = 0;
+    a.elem = nullptr;
+}
 
-template<class T, class A>
-vector<T, A>& vector<T, A>::operator=(vector&& a)
+template <class T, class A>
+vector<T, A> &vector<T, A>::operator=(vector &&a)
 {
     delete[] elem;
     elem = a.elem;
@@ -102,35 +99,39 @@ vector<T, A>& vector<T, A>::operator=(vector&& a)
     return *this;
 }
 
-template<class T, class A>
+template <class T, class A>
 void vector<T, A>::reserve(int newalloc)
 {
-    if (newalloc <= this->space) return;
-    vector_base<T, A> b(this->alloc, newalloc);
-    std::uninitialized_copy(b.elem, &b.elem[this->sz], this->elem);
+    if (newalloc <= space)
+        return;
     T* p = alloc.allocate(newalloc);
+    for (int i = 0; i < sz; ++i)
+        alloc.construct(&p[i], elem[i]);
 
-    for (int i = 0; i < this->sz; ++i) 
-        alloc.destroy(&this->elem[i]);
-    std::swap<vector_base<T, A>>(*this, b);
+    for (int i = 0; i < sz; ++i)
+        alloc.destroy(&elem[i]);
+
+    alloc.deallocate(elem, space);
+    elem = p;
+    space = newalloc;
 }
 
-template<class T, class A>
+template <class T, class A>
 int vector<T, A>::capacity() const { return space; }
 
-template<class T, class A>
+template <class T, class A>
 void vector<T, A>::resize(int newsize, T val)
 {
     reserve(newsize);
     for (int i = sz; i < newsize; ++i)
         alloc.construct(&elem[i], val);
     for (int i = newsize; i < sz; ++i)
-        alloc.destroy(&elem[i]);
+        alloc.destroy(elem[i]);
     sz = newsize;
 }
 
-template<class T, class A>
-void vector<T, A>::push_back(const T& val)
+template <class T, class A>
+void vector<T, A>::push_back(const T &val)
 {
     if (space == 0)
         reserve(8);
@@ -140,10 +141,11 @@ void vector<T, A>::push_back(const T& val)
     ++sz;
 }
 
-template<class T, class A>
-vector<T, A>& vector<T, A>::operator=(const vector& a)
+template <class T, class A>
+vector<T, A> &vector<T, A>::operator=(const vector &a)
 {
-    if (this == &a) return *this;
+    if (this == &a)
+        return *this;
 
     if (a.sz <= space)
     {
@@ -152,7 +154,7 @@ vector<T, A>& vector<T, A>::operator=(const vector& a)
         sz = a.sz;
         return *this;
     }
-    T* p = new T[a.sz];
+    T *p = new T[a.sz];
     for (int i = 0; i < a.sz; ++i)
         p[i] = a.elem[i];
     delete[] elem;
@@ -161,18 +163,18 @@ vector<T, A>& vector<T, A>::operator=(const vector& a)
     return *this;
 }
 
-
-template<class T, class A>
-T& vector<T, A>::at(int n)
+template <class T, class A>
+T &vector<T, A>::at(int n)
 {
-    if (n < 0 || sz <= n) throw std::out_of_range();
+    if (n < 0 || sz <= n)
+        throw std::out_of_range("out of range");
     return elem[n];
 }
 
-template<class T, class A>
-const T& vector<T, A>::at(int n) const
+template <class T, class A>
+const T &vector<T, A>::at(int n) const
 {
-    if (n < 0 || sz <= n) throw std::out_of_range();
+    if (n < 0 || sz <= n)
+        throw std::out_of_range("out of range");
     return elem[n];
 }
-
